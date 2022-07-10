0f5fe13fc7c22919c33f5450dcab306f807b73ef
RomanMuraro <roman.muraro@gmail.com>
2022-07-11 04:32:31 +1000

    Добавление основной функции и вызов побчных

--- ThreeFunctions.cpp
+++ ThreeFunctions.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
+
+
+	
+
+
+int fact(int a){
+	int cd = 1;
+	for(int i=1;i<=a;i++){
+		cd = cd*i
+	}
+return cd;}
+
+int main(){
+	int b;
+	int c;
+	cin>>b;
+	c = b;
+	b = fact(b);
+	c = sqrt(c);
+	cout<<" Factorial is "<<b<<", Sqrt is "<<c;
+	
+return 0;}
